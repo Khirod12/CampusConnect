1e7918d4cc943d665137906f7e554c1935397b87
Khirod kumar Patra <patrakhirodkumar561@gmail.com>
2026-02-21 17:34:26 +0530

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,13 +1,13 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, subject, text) => {
-
   try {
 
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
+      family: 4,   // 👈 VERY IMPORTANT (forces IPv4)
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
